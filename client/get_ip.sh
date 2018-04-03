c962e94469650abce0d104b216ebd3c3b18b604d
Benjamin Schmid <git@schmidb.ch>
2018-04-03 21:28:41 +0200

    hide stderr of curl

--- client/get_ip.sh
+++ client/get_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source `dirname $0`/config.sh
-curl -u $username:$password $server'get.php'
+curl -u $username:$password $server'get.php' 2> /dev/null
--- client/set_ip.sh
+++ client/set_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source `dirname $0`/config.sh
-curl -u $username:$password -d "phrase=$phrase" -X POST $server'set.php'
+curl -u $username:$password -d "phrase=$phrase" -X POST $server'set.php' 2> /dev/null
